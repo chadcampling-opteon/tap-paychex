ed92099b5efcac40217aaf95d03106560afd1613
chad.campling <chad.campling@cubetec.com>
2025-03-30 13:44:20 +1100

    Added streams from Paychex Time API for Users and Time Off Requests

--- tap_paychex/auth.py
+++ tap_paychex/auth.py
@@ -1,9 +1,15 @@
 """Paychex Authentication."""
 
 from __future__ import annotations
+import json
+import typing as t
+import requests
+from singer_sdk.authenticators import OAuthAuthenticator, APIAuthenticatorBase, SingletonMeta
 
-from singer_sdk.authenticators import OAuthAuthenticator, SingletonMeta
+if t.TYPE_CHECKING:
+    import logging
 
+    from singer_sdk.streams.rest import _HTTPStream
 
 class PaychexAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
     """Authenticator class for Paychex."""
@@ -39,3 +45,84 @@ class PaychexAuthenticator(OAuthAuthenticator, metaclass=SingletonMeta):
             auth_endpoint="https://api.paychex.com/auth/oauth/v2/token",
             oauth_scopes="",
         )
+
+class PaychexTimeAuthenticator(APIAuthenticatorBase):
+    """Implements API key authentication for REST Streams.
+
+    This authenticator will merge a key-value pair with either the
+    HTTP headers or query parameters specified on the stream. Common
+    examples of key names are "x-api-key" and "Authorization" but
+    any key-value pair may be used for this authenticator.
+    """
+    _BODY_KEY = "AuthToken"
+    auth_credentials = {}
+    
+    def __init__(
+        self,
+        stream: _HTTPStream
+    ) -> None:
+        """Create a new authenticator.
+
+        Args:
+            stream: The stream instance to use with this authenticator.
+            key: API key parameter name.
+            value: API key value.
+            location: Where the API key is to be added. Either 'header' or 'params'.
+
+        Raises:
+            ValueError: If the location value is not 'header' or 'params'.
+        """
+        super().__init__(stream=stream)
+        self.auth_credentials = {self._BODY_KEY: None}
+        
+    @classmethod
+    def create_for_stream(
+        cls: type[PaychexTimeAuthenticator],
+        stream: _HTTPStream,
+    ) -> PaychexTimeAuthenticator:
+        """Create an Authenticator object specific to the Stream class.
+
+        Args:
+            stream: The stream instance to use with this authenticator.
+            key: API key parameter name.
+            value: API key value.
+            location: Where the API key is to be added. Either 'header' or 'params'.
+
+        Returns:
+            APIKeyAuthenticator: A new
+                :class:`singer_sdk.authenticators.APIKeyAuthenticator` instance.
+        """
+        return cls(stream=stream)
+    
+    def authenticate_request(self, request):
+        if not self.auth_credentials.get(self._BODY_KEY):
+            self.create_token()
+            
+        body_dict = json.loads(request.body)
+        body_dict.update(self.auth_credentials)
+        request.body = json.dumps(body_dict)
+        return super().authenticate_request(request)
+    
+    def create_token(self):
+        auth_request_payload = {
+            "CustomerAlias": self.config["time_customer_alias"],
+            "SharedKey": self.config["time_shared_key"],
+            "UserName": "wsuser",
+            "UserPass": self.config["time_password"],
+        }
+        token_response = requests.post(
+            "https://paychex.centralservers.com/service/ws-json/2.0/CreateToken",
+            json=auth_request_payload,
+            timeout=60
+        )
+        
+        try:
+            token_response.raise_for_status()
+        except requests.HTTPError as ex:
+            msg = f"Failed OAuth login, response was '{token_response.json()}'. {ex}"
+            raise RuntimeError(msg) from ex
+
+        self.logger.info("Authorization attempt was successful.")
+
+        token = token_response.json()
+        self.auth_credentials[self._BODY_KEY] = token
--- tap_paychex/streams.py
+++ tap_paychex/streams.py
@@ -2,11 +2,15 @@
 
 from __future__ import annotations
 
+from datetime import datetime, timezone
+import decimal
 import typing as t
 from importlib import resources
 from urllib.parse import urlparse
+from dateutil.relativedelta import relativedelta
+from singer_sdk.helpers.jsonpath import extract_jsonpath
 
-from tap_paychex.client import PaychexStream
+from tap_paychex.client import PaychexStream, PaychexTimePaginator, PaychexTimeStream
 
 
 SCHEMAS_DIR = resources.files(__package__) / "schemas"
@@ -72,4 +76,76 @@ class WorkersCommunicationStream(PaychexStream):
     state_partitioning_keys = []
 
     def get_url(self, context):
-        return context["workers_communication_url"]
+        return context["workers_communication_url"]    
+    
+    def post_process(self, row, context = None):
+        row["workerId"] = context["workerId"]
+        return row
+
+class TimeOffRequest(PaychexTimeStream):
+
+    name = "time_off_requests"
+    path = "/GetUserTimeOffRequest"
+    pagination_support = True
+    
+    schema_filepath = SCHEMAS_DIR / "time_off_requests.json"
+    
+    primary_keys: t.ClassVar[list[str]] = ["ID"]
+    replication_key = "ExtractedDateTime"
+    
+    def prepare_request_payload(self, context, next_page_token):
+        if not next_page_token:
+            bookmark = self.get_starting_timestamp(context)
+            if not bookmark:
+                bookmark = (datetime.now(timezone.utc))
+            
+            start_date = (bookmark - relativedelta(days=59)).replace(hour=0, minute=0, second=0, microsecond=0)
+            end_date = bookmark.replace(hour=0, minute=0, second=0, microsecond=0)
+        else:
+            start_date= next_page_token["start_date"]
+            end_date= next_page_token["end_date"]
+            
+        return {
+            "StartDate":f"/Date({int(start_date.timestamp()) * 1000}+0000)/",
+            "EndDate":f"/Date({int(end_date.timestamp()) * 1000}+0000)/",
+            "DateTimeSchema":0,
+            "IgnoreDeletedRequests":False,
+            "IgnoreDetails":True,
+            "DataAction":{
+                "Name":"SELECT-ALL"
+            }
+        }
+        
+    def get_new_paginator(self):
+            return PaychexTimePaginator(day_count_window=59, day_count_future=180)
+            
+    def post_process(self, row, context = None):
+        row['ExtractedDateTime'] = datetime.now(timezone.utc).isoformat(timespec='seconds')
+        row['EndDateTime'] = row['EndDateTimeSchema']
+        del row['EndDateTimeSchema']
+        row['StartDateTime'] = row['StartDateTimeSchema']
+        del row['StartDateTimeSchema']
+        row['DateTimeSubmitted'] = datetime.fromtimestamp(int(row['DateTimeSubmitted'][6:16]), tz=timezone.utc)
+        return row
+        
+class TimeUsers(PaychexTimeStream):
+
+    name = "time_system_users"
+    path = "/GetUserBasic"
+    pagination_support = False
+    
+    schema_filepath = SCHEMAS_DIR / "time_users.json"
+    
+    primary_keys: t.ClassVar[list[str]] = ["EmpIdentifier"]
+    
+    def prepare_request_payload(self, context, next_page_token):
+        return {
+            "EffectiveDate":f"/Date({int(datetime.now(timezone.utc).timestamp()) * 1000}+0000)/",
+            "DataAction":{
+                "Name":"SELECT-ALL"
+            }
+        }
+        
+    def post_process(self, row, context = None):
+        row['Email'] = None if row.get('Email') == '' else row.get('Email')
+        return row
